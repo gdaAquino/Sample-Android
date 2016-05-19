5b0a0f20166284b5bc6863ffca5a92c9fa510b2d
Gian Darren Aquino <gda.i.aquino@gmail.com>
2016-05-19 10:11:23 +0800

    convert to lambda expressions

--- app/src/main/java/com/giaquino/sample/model/UserModel.java
+++ app/src/main/java/com/giaquino/sample/model/UserModel.java
@@ -7,10 +7,7 @@ import com.giaquino.sample.model.entity.User;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
 import rx.Single;
-import rx.functions.Action1;
-import rx.functions.Func1;
 
 /**
  * @author Gian Darren Azriel Aquino.
@@ -34,34 +31,23 @@ public class UserModel {
     }
 
     public Single<List<User>> getUsersFromLocal() {
-        return Single.fromCallable(new Callable<List<User>>() {
-            @Override
-            public List<User> call() throws Exception {
-                if (cache != null) {
-                    return cache;
-                }
-                cache = UserContract.getUsers(database);
+        return Single.fromCallable(() -> {
+            if (cache != null) {
                 return cache;
             }
+            cache = UserContract.getUsers(database);
+            return cache;
         });
     }
 
     public Single<List<User>> getUsersFromNetwork(final int since) {
         return api.getUsers("ef085af14ec8e343d27c8a329d66a5ae15fc6b92", since) //dont hardcode token
-                  .map(new Func1<List<User>, List<User>>() {
-                      @Override
-                      public List<User> call(List<User> users) {
-                          return Collections.unmodifiableList(users);
-                      }
-                  })
-            .doOnSuccess(new Action1<List<User>>() {
-                @Override
-                public void call(List<User> users) {
-                    if (since == SINCE_INITIAL_VALUE) {
-                        cache = users;
-                        UserContract.insertUsers(database, users);
-                    }
+            .map(Collections::unmodifiableList)
+            .doOnSuccess(users -> {
+                if (since == SINCE_INITIAL_VALUE) {
+                    cache = users;
+                    UserContract.insertUsers(database, users);
                 }
-                  });
+            });
     }
 }
--- app/src/main/java/com/giaquino/sample/ui/users/presenter/UsersPresenter.java
+++ app/src/main/java/com/giaquino/sample/ui/users/presenter/UsersPresenter.java
@@ -1,15 +1,10 @@
 package com.giaquino.sample.ui.users.presenter;
 
 import com.giaquino.sample.common.app.Presenter;
-import com.giaquino.sample.model.entity.User;
 import com.giaquino.sample.model.UserModel;
 import com.giaquino.sample.ui.users.view.UsersView;
 
-import java.util.List;
-
 import rx.Subscription;
-import rx.functions.Action0;
-import rx.functions.Action1;
 
 /**
  * @author Gian Darren Azriel Aquino.
@@ -26,41 +21,17 @@ public class UsersPresenter extends Presenter<UsersView> {
         Subscription subscription = model.getUsersFromLocal()
             .concatWith(model.getUsersFromNetwork(UserModel.SINCE_INITIAL_VALUE))
             .subscribe(
-                new Action1<List<User>>() { //success
-                    @Override
-                    public void call(List<User> users) {
-                        view().setUsers(users);
-                    }
-                },
-                new Action1<Throwable>() { //failure
-                    @Override
-                    public void call(Throwable throwable) {
-                        view().showErrorMessage("error : " + throwable);
-                    }
-                },
-                new Action0() { //completed
-                    @Override
-                    public void call() {
-                    }
-                });
+                users -> view().setUsers(users), //success
+                throwable -> view().showErrorMessage("error : " + throwable), //error
+                () -> {}); //completed
         addSubscriptionToUnsubscribe(subscription);
     }
 
     public void loadNextUsers(int since) {
         Subscription subscription = model.getUsersFromNetwork(since)
             .subscribe(
-                new Action1<List<User>>() {
-                    @Override
-                    public void call(List<User> users) { //success
-                        view().addUsers(users);
-                    }
-                },
-                new Action1<Throwable>() { //failure
-                    @Override
-                    public void call(Throwable throwable) {
-                        view().showErrorMessage("error : " + throwable);
-                    }
-                });
+                users -> view().addUsers(users), //success
+                throwable -> view().showErrorMessage("error : " + throwable)); //error
         addSubscriptionToUnsubscribe(subscription);
     }
 }
